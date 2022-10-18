1cc21bdb8d4e8da6bbfa8ae5bfd503c5c316121b
Aleksandr Deev <geshtait@mail.ru>
2022-10-18 22:04:55 +0500

    Сделал ДЗ ООР часть 1 задание 1 и 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human maksim = new Human("Максим", "Минск", 1987, "бренд-менеджер");
+        Human ania = new Human("Аня", "Москва", 1993, "методист образовательных программ");
+        Human katya = new Human("Катя", "Калининград", 1994, "продакт-менеджер");
+        Human artem = new Human("Артём", "Москва", 1995, "директор по развитию бизнеса");
+
+        maksim.printingData();
+        ania.printingData();
+        katya.printingData();
+        artem.printingData();
     }
 }
